feb975e08152483e60f06ef70e6653c3ce505be6
“Florent <f.losada@hotmail.fr>
2019-04-26 16:52:52 +0200

    mainactivity

--- app/src/main/java/com/dtc/myapplication/MainActivity.java
+++ app/src/main/java/com/dtc/myapplication/MainActivity.java
@@ -1,13 +1,67 @@
 package com.dtc.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.dtc.myapplication.model.Employee;
+import com.dtc.myapplication.model.EmployeeList;
+import com.dtc.myapplication.network.EmployeeAdapter;
+import com.dtc.myapplication.network.GetEmployeeDataService;
+import com.dtc.myapplication.network.RetrofitInstance;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EmployeeAdapter adapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Créer un identifiant pour l'interface RetrofitInstance
+        GetEmployeeDataService service = RetrofitInstance.getRetrofitInstance().create(GetEmployeeDataService.class);
+
+        // Appelez la méthode avec paramètre dans l'interface pour obtenir les données sur l'employé
+        Call<EmployeeList> call = service.getEmployeeData(100);
+
+        // Journalise l'URL appelée
+        Log.wtf("URL Called", call.request().url() + "");
+
+        call.enqueue(new Callback<EmployeeList>() {
+            @Override
+            public void onResponse(Call<EmployeeList> call, Response<EmployeeList> response) {
+                generateEmployeeList(response.body().getEmployeeArrayList());
+            }
+
+            @Override
+            public void onFailure(Call<EmployeeList> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Méthode pour générer la liste des employés utilisant RecyclerView avec un adaptateur personnalisé
+    private void generateEmployeeList(ArrayList<Employee> empDataList) {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_employee_list);
+
+        adapter = new EmployeeAdapter(empDataList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
     }
 }
